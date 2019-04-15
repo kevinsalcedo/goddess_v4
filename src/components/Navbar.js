ad48bfcdab2b95e4bc013fea32977bb465a0156b
Kevin Salcedo <kevsalcedo@gmail.com>
2019-04-14 19:53:12 -0500

    changed menu type

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,34 +11,32 @@ class Navbar extends React.Component {
     const { activeItem } = this.state;
 
     return (
-      <div>
-        <Menu pointing secondary>
-          <Menu.Item as={Link} to='/' name='logo' />
-          <Menu.Menu position='right'>
-            <Menu.Item
-              as={Link}
-              to='/posts'
-              name='blog'
-              active={activeItem === "blog"}
-              onClick={this.handleItemClick}
-            />
-            <Menu.Item
-              as={Link}
-              to='#'
-              name='photos'
-              active={activeItem === "photos"}
-              onClick={this.handleItemClick}
-            />
-            <Menu.Item
-              as={Link}
-              to='#'
-              name='about'
-              active={activeItem === "about"}
-              onClick={this.handleItemClick}
-            />
-          </Menu.Menu>
-        </Menu>
-      </div>
+      <Menu pointing>
+        <Menu.Item as={Link} to='/' name='logo' />
+        <Menu.Menu position='right'>
+          <Menu.Item
+            as={Link}
+            to='/posts'
+            name='blog'
+            active={activeItem === "blog"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            as={Link}
+            to='#'
+            name='photos'
+            active={activeItem === "photos"}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            as={Link}
+            to='#'
+            name='about'
+            active={activeItem === "about"}
+            onClick={this.handleItemClick}
+          />
+        </Menu.Menu>
+      </Menu>
     );
   }
 }
