53c45bccc10b14414546344eda518b91ff935025
Kevin Salcedo <kevsalcedo@gmail.com>
2019-05-08 14:34:37 -0500

    added basic slate.js editor

--- src/components/posts/PostCreate.js
+++ src/components/posts/PostCreate.js
@@ -1,9 +1,46 @@
 import React from "react";
-import { Container } from "semantic-ui-react";
+import { Container, Header } from "semantic-ui-react";
+import { Editor } from "slate-react";
+import { Value } from "slate";
+
+// Initial content
+const initialValue = Value.fromJSON({
+  document: {
+    nodes: [
+      {
+        object: "block",
+        type: "paragraph",
+        nodes: [
+          {
+            object: "text",
+            leaves: [
+              {
+                text: "A line of text in a paragraph."
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+});
 
 class PostCreate extends React.Component {
+  state = {
+    value: initialValue
+  };
+
+  onChange = ({ value }) => {
+    this.setState({ value });
+  };
+
   render() {
-    return <Container>PostCreate</Container>;
+    return (
+      <Container>
+        <Header>Post Create</Header>
+        <Editor value={this.state.value} onChange={this.onChange} />
+      </Container>
+    );
   }
 }
 export default PostCreate;
