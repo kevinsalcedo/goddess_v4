1d50b88fe8d8e75b500c9978170f32f3ac411313
Kevin Salcedo <kevsalcedo@gmail.com>
2019-05-14 11:59:42 -0500

    added firebase

--- src/components/posts/PostCreate.js
+++ src/components/posts/PostCreate.js
@@ -1,12 +1,48 @@
 import React from "react";
-import { Container, Header } from "semantic-ui-react";
+import { Container, Header, Button, Input, Form } from "semantic-ui-react";
 import RichText from "./editor/RichText";
+import firebase from "../../firestore";
 
 class PostCreate extends React.Component {
+  state = {
+    title: "",
+    content: "Sample"
+  };
+
+  handleTitleChange = e => {
+    this.setState({ title: e.target.value });
+  };
+
+  addPost = e => {
+    e.preventDefault();
+    const db = firebase.firestore();
+    db.settings({
+      timestampsInSnapshots: true
+    });
+
+    const postRef = db.collection("posts").add({
+      title: this.state.title,
+      content: this.state.content
+    });
+
+    this.setState({
+      title: "",
+      content: "New Sample"
+    });
+  };
   render() {
     return (
       <Container>
-        <Header as='h1'>Post Create</Header>
+        <Form onSubmit={this.addPost}>
+          <Header as='h1'>Post Create</Header>
+          <Input
+            name='title'
+            value={this.state.title}
+            onChange={this.handleTitleChange}
+          />
+
+          <Button type='submit'>Submit</Button>
+        </Form>
         <RichText />
       </Container>
     );
